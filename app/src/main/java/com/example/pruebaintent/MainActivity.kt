41214b903416deeddcfd998b041e6a100bf22dcc
yagsim <yagosimofernandez@gmail.com>
2023-01-23 11:29:10 +0100

    Intents 2

--- app/src/main/java/com/example/pruebaintent/MainActivity.kt
+++ app/src/main/java/com/example/pruebaintent/MainActivity.kt
@@ -2,79 +2,80 @@ package com.example.pruebaintent
 
 import android.app.Activity
 import android.content.Intent
+import android.graphics.Bitmap
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
-    val RESULTADO_UNO = 1
-    val RESULTADO_DOS = 2
-    val RESULTADO_TRES = 3
 
+class MainActivity : AppCompatActivity() {
+    private val RESULTADO_UNO = 1
+    private val REQUEST_IMAGE_CAPTURE = 2
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navegador: Button= findViewById(R.id.navegador)
-        navegador.setOnClickListener{
-            val navegador=Intent(Intent.ACTION_VIEW)
-            val url="http://www.google.com"
-            navegador.data= Uri.parse(url)
-            startActivity(navegador)
-        }
-        val llamar: Button= findViewById(R.id.llamar)
-        llamar.setOnClickListener{
-            val intent = Intent(Intent.ACTION_DIAL)
-            // rellenamos el intent con el número
-            intent.data = Uri.parse("tel:" + llamar.text)
-            // llamamos a la activity treléfono
-            startActivity(intent)
+
+
+        val camara: Button = findViewById(R.id.camara)
+        camara.setOnClickListener() {
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(intent, REQUEST_IMAGE_CAPTURE)
 
         }
+
+
         val boton: Button = findViewById(R.id.button)
-        boton.setOnClickListener() {
+        boton.setOnClickListener {
             val intent = Intent(this, SecondActivity::class.java)
-            intent.putExtra("n1", 6)
-            intent.putExtra("n2", 2)
-            startActivityForResult(intent, RESULTADO_UNO)   //RESULTADO_UNO es el requestCode que recibe abajo
-            intent.putExtra("numero1", 4)
-            intent.putExtra("numero2", 9)
-            startActivityForResult(intent, RESULTADO_DOS)
-            intent.putExtra("numero1", 1)
-            intent.putExtra("numero2", 5)
-            startActivityForResult(intent, RESULTADO_TRES)
-        }
+            val random = kotlin.random.Random
+            intent.putExtra("numero1", random.nextInt(0,100))
+            intent.putExtra("numero2", random.nextInt(0,100))
+            startActivityForResult(intent,RESULTADO_UNO)   //RESULTADO_UNO es el requestCode que recibe abajo
 
+        }
     }
 
-    @Deprecated("Deprecated a partir de API 30")
-    // recoje el intent cuando hacemos setResult() en la SecondActivity
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {  //request=ResultadoUno,resultCodeResultOk
-        super.onActivityResult(requestCode, resultCode, data)
-        // Definimos el text view para mostrar el dato que nos manda la Second
-        val sumaUno = findViewById<TextView>(R.id.resultado1)
-        val sumaDos = findViewById<TextView>(R.id.resultado2)
-        val sumaTres = findViewById<TextView>(R.id.resultado3)
-        // Comprueba que el resultado es OK
-        if (resultCode != Activity.RESULT_OK ||data==null) return
-        // Puedo distinguir diferentes acciones según el requestCode
+
+        @Deprecated("Deprecated a partir de API 30")
+        // recoje el intent cuando hacemos setResult() en la SecondActivity
+        override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {  //request=ResultadoUno,resultCodeResultOk
+            super.onActivityResult(requestCode, resultCode, data)
+            val imageView = findViewById<ImageView>(R.id.imageView)
+            // Definimos el text view para mostrar el dato que nos manda la Second
+
+
+            // Comprueba que el resultado es OK
+            if (resultCode != Activity.RESULT_OK || data == null) return   //debemos recoger el data
+            // Puedo distinguir diferentes acciones según el requestCode
             when (requestCode) {
                 RESULTADO_UNO -> {
-                    // si el intent no es null muestro el resultado
-                    sumaUno.text = data.getIntExtra("suma",0).toString()
+                    val sumaUno = findViewById<TextView>(R.id.resultado1)//recogemos textView
+                    val num1 = data.getIntExtra("num1", 0)
+                    val num2 = data.getIntExtra("num2", 0)
+                    val suma = num1 + num2
+                    val resIntro = data.getIntExtra("suma", 0)
+
+                    //comprobación de la suma manual
+                    if (suma == resIntro) {
+                        sumaUno.text = "SUMA CORRECTA"
+                    } else {
+                        sumaUno.text = "SUMA INCORRECTA"
+                    }
                 }
-                RESULTADO_DOS -> {
-                    // si el intent no es null muestro el resultado
-                    sumaDos.text = data.getIntExtra("suma",0).toString()
+                REQUEST_IMAGE_CAPTURE->{
+                    val imageBitmap = data.extras!!.get("data") as Bitmap
+                    imageView.setImageBitmap(imageBitmap)
                 }
-                RESULTADO_TRES -> {
-                    // si el intent no es null muestro el resultado
-                    sumaTres.text = data.getIntExtra("suma",0).toString()
-                }
+
                 // Other result codes
                 else -> {}
             }
 
+        }
+
     }
-}
+
--- app/src/main/java/com/example/pruebaintent/SecondActivity.kt
+++ app/src/main/java/com/example/pruebaintent/SecondActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 
 class SecondActivity : AppCompatActivity() {
@@ -17,18 +18,32 @@ class SecondActivity : AppCompatActivity() {
         // Recoge el Intent que ha iniciado la actividad
         val intent = getIntent()
 
-        // Recoge el valor de tipo String pasado en el Intent
-        val num1= intent.getIntExtra("n1", 0)
-        val num2= intent.getIntExtra("n2",0)
+        // En cada text view inserto el numero aleatorio 1 y el 2
+        val valUno = findViewById<TextView>(R.id.val1)
+        val num1= intent.getIntExtra("numero1", 0)  //recojo el numero1 del main
+        valUno.text=num1.toString()
 
-        // Meto un nuevo dato en el intent
-        intent.putExtra("suma",num1+num2);
+        val valDos = findViewById<TextView>(R.id.val2)
+        val num2= intent.getIntExtra("numero2",0)//recojo el numero2 del main
+        valDos.text=num2.toString()
 
-        setResult(Activity.RESULT_OK, intent);   //una vez echo esto salta el metodo onActivityForResult
+        val resIntro=findViewById<EditText>(R.id.SUMA)
 
-        // cierro la activity
-        finish()
+
+
+        val boton: Button = findViewById(R.id.button2)  //al darle al boton compruebo
+        boton.setOnClickListener {
+            val res : Int = resIntro.text.toString().toInt()
+            intent.putExtra("sumaIntro", res)
+
+            setResult(Activity.RESULT_OK, intent);   //una vez echo esto salta el metodo onActivityForResult
+            // cierro la activity
+            finish()
+        }
+
+
     }
+
 }
 
 
